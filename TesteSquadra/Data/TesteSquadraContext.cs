473b5a34d7c4eb92683303e22977ecae1e8712e1
Diego Oliveira Serra <dieos2@gmail.com>
2020-03-09 22:39:36 -0300

    inicial

---
+++ TesteSquadra/Data/TesteSquadraContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TesteSquadra.Models;
+
+    public class TesteSquadraContext : DbContext
+    {
+        public TesteSquadraContext (DbContextOptions<TesteSquadraContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TesteSquadra.Models.BlogPost> BlogPost { get; set; }
+    }
